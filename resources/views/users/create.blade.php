ce814d87dc0a9f42d6f7b578415a1bf9927dbe73
LA4M <leonardoaranedam@gmail.com>
2022-04-07 18:02:04 -0400

    users/create

---
+++ resources/views/users/create.blade.php
@@ -0,0 +1,51 @@
+@extends('layouts.main', ['activePage' => 'users', 'titlePage' => 'Nuevo Usuario'])
+@section('content')
+<div class="content">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <form action="{{route('users.store')}}" method="post" class="form-horizontal">
+                    @csrf
+                    <div class="card">
+                        <div class="card-header card-header-primary">
+                            <h4 class="card-tittle">Usuario</h4>
+                            <p class="card-category">Ingresar datos</p>
+                        </div>
+                        <div class="card-body">
+                            <div class="row">
+                                <label for="name" class="col-sm-2 col-form-label">Nombre</label>
+                                <div class="col-sm-7">
+                                    <input type="text" class="form-control" name="name" placeholder="Ingrese el nombre" autofocus>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <label for="rut" class="col-sm-2 col-form-label">RUT</label>
+                                <div class="col-sm-7">
+                                    <input type="text" class="form-control" name="rut" placeholder="Ingrese el RUT">
+                                </div>
+                            </div>
+                            <div class="row">
+                                <label for="email" class="col-sm-2 col-form-label">Correo</label>
+                                <div class="col-sm-7">
+                                    <input type="email" class="form-control" name="email" placeholder="Ingrese el correo" >
+                                </div>
+                            </div>
+                            <div class="row">
+                                <label for="password" class="col-sm-2 col-form-label">Contraseña</label>
+                                <div class="col-sm-7">
+                                    <input type="password" class="form-control" name="password" placeholder="Contraseña" >
+                                </div>
+                            </div>
+                        </div>
+                        <!--footer-->
+                        <div class="card-footer ml-auto mr-auto">
+                            <button type="submit" class="btn btn-primary">{{ __('Guardar') }}</button>
+                        </div>
+                        <!--End footer-->
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
