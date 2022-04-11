5eb772cdd7ebe4e3a40b3b51a6f166652d374306
nico_sqd <nicolasquintraman@gmail.com>
2022-04-11 00:57:47 -0400

    Implementacion view editar(actualizar)

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -28,4 +28,34 @@ class UserController extends Controller
         return redirect()->action([UserController::class, 'index'])->with('success', 'Usuario creado correctamente.');
     }
 
+    public function Show(User $user)
+    {
+        //$user = User::findOrFail($id);
+        //dd($user);
+        return view('users.show', compact('user'));
+    }
+
+    public function edit(User $user)
+    {
+        //$user = User::findOrFail($id);
+        //dd($user);
+        return view('users.edit', compact('user'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $user=User::findOrFail($id);
+        $data = $request->only('name','username','email');
+        if(trim($request->password)=='')
+        {
+            $data=$request->except('password');
+        }
+        else {
+            $data=$request->all();
+            $data['password']=bcrypt($request->password);
+        }
+        $user->update($data);
+        return redirect()->back();
+    }
+
 }
